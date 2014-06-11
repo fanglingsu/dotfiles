24c17c62d4828f1691421b338380190b2f46309a
Daniel Carl <danielcarl@gmx.de>
2014-06-11 09:55:25 +0200

    Made scratchpad titled window floating.

--- build/dwm/config.h
+++ build/dwm/config.h
@@ -31,18 +31,19 @@ static const char *tags[] = { "term", "dev", "web", "media", "jabber", "mail" };
 static unsigned int scratchtag = 1 << LENGTH(tags);
 
 static const Rule rules[] = {
-	/* xprop(1):
-	 *	WM_CLASS(STRING) = instance, class
-	 *	WM_NAME(STRING) = title
-	 */
-	/* class                instance    title       tags mask     isfloating  monitor */
-	{ "Gimp",               NULL,       NULL,       0,            True,       -1 },
-	{ "Wicd-client.py",     NULL,       NULL,       0,            True,       -1 },
-    { "Vidalia",            NULL,       NULL,       0,            True,       -1 },
-    { "Tabbed",             NULL,       NULL,       1 << 2,       False,      -1 },
-	{ "Chromium-browser",   NULL,       NULL,       1 << 2,       False,      -1 },
-    { "jetbrains-phpstorm", NULL,       NULL,       1 << 1,       False       -1 },
-    { "Display",            NULL,       NULL,       0,            True,       -1 },
+    /* xprop(1):
+     *    WM_CLASS(STRING) = instance, class
+     *    WM_NAME(STRING) = title
+     */
+    /* class                instance    title         tags mask     isfloating  monitor */
+    { "Gimp",               NULL,       NULL,         0,            True,       -1 },
+    { "Wicd-client.py",     NULL,       NULL,         0,            True,       -1 },
+    { "Vidalia",            NULL,       NULL,         0,            True,       -1 },
+    { "Tabbed",             NULL,       NULL,         1 << 2,       False,      -1 },
+    { "Chromium-browser",   NULL,       NULL,         1 << 2,       False,      -1 },
+    { "jetbrains-phpstorm", NULL,       NULL,         1 << 1,       False       -1 },
+    { "Display",            NULL,       NULL,         0,            True,       -1 },
+    { NULL,                 NULL,       "scratchpad", 0,            True,       -1 },
 };
 
 /* layout(s) */
@@ -51,20 +52,20 @@ static const int nmaster      = 1;     /* number of clients in master area */
 static const Bool resizehints = False; /* True means respect size hints in tiled resizals */
 
 static const Layout layouts[] = {
-	/* symbol     arrange function */
-	{ "[T]",      tile },
-	{ "[B]",      bstack },
-	{ "[M]",      monocle },
-	{ NULL,       NULL },
+    /* symbol     arrange function */
+    { "[T]",      tile },
+    { "[B]",      bstack },
+    { "[M]",      monocle },
+    { NULL,       NULL },
 };
 
 /* key definitions */
 #define MODKEY Mod1Mask
 #define TAGKEYS(KEY,TAG) \
-	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
-	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
-	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
-	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
+    { MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
+    { MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
+    { MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
+    { MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
@@ -78,61 +79,61 @@ static const char *slockcmd[] = { "slock", NULL };
 static const char *scratchpadcmd[] = { terminal, "-name", scratchpadname, "-geometry", "80x20", NULL };
 
 static Key keys[] = {
-	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY|ShiftMask,             XK_z,      spawn,          {.v = slockcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-	{ MODKEY|ShiftMask,             XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY|ShiftMask,             XK_b,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY|ShiftMask,             XK_m,      setlayout,      {.v = &layouts[2]} },
+    /* modifier                     key        function        argument */
+    { MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+    { MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+    { MODKEY|ShiftMask,             XK_z,      spawn,          {.v = slockcmd } },
+    { MODKEY,                       XK_b,      togglebar,      {0} },
+    { MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
+    { MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
+    { MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
+    { MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
+    { MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
+    { MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+    { MODKEY,                       XK_Return, zoom,           {0} },
+    { MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
+    { MODKEY|ShiftMask,             XK_t,      setlayout,      {.v = &layouts[0]} },
+    { MODKEY|ShiftMask,             XK_b,      setlayout,      {.v = &layouts[1]} },
+    { MODKEY|ShiftMask,             XK_m,      setlayout,      {.v = &layouts[2]} },
     { MODKEY,                       XK_x,      togglescratch,  {.v = scratchpadcmd } },
-	{ MODKEY,                       XK_t,      togglefloating, {0} },
-	{ MODKEY,                       XK_space,  nextlayout,     {0} },
-	{ MODKEY|ShiftMask,             XK_space,  prevlayout,     {0} },
+    { MODKEY,                       XK_t,      togglefloating, {0} },
+    { MODKEY,                       XK_space,  nextlayout,     {0} },
+    { MODKEY|ShiftMask,             XK_space,  prevlayout,     {0} },
     { MODKEY|ShiftMask,             XK_j,      pushdown,       {0} },
     { MODKEY|ShiftMask,             XK_k,      pushup,         {0} },
     { MODKEY,                       XK_m,      focusmaster,    {0} },
     { MODKEY,                       XK_u,      focusurgent,    {0} },
     { MODKEY,                       XK_Tab,    view,           {0} },
     /*
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+    { MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
+    { MODKEY,                       XK_period, focusmon,       {.i = +1 } },
+    { MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
+    { MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
     */
-	TAGKEYS(                        XK_1,                      0)
-	TAGKEYS(                        XK_2,                      1)
-	TAGKEYS(                        XK_3,                      2)
-	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
+    TAGKEYS(                        XK_1,                      0)
+    TAGKEYS(                        XK_2,                      1)
+    TAGKEYS(                        XK_3,                      2)
+    TAGKEYS(                        XK_4,                      3)
+    TAGKEYS(                        XK_5,                      4)
+    TAGKEYS(                        XK_6,                      5)
     /*
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
+    TAGKEYS(                        XK_7,                      6)
+    TAGKEYS(                        XK_8,                      7)
+    TAGKEYS(                        XK_9,                      8)
     */
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
+    { MODKEY,                       XK_0,      view,           {.ui = ~0 } },
+    { MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
     /*
-	{ MODKEY|ControlMask,           XK_0,      toggleview,     {.ui = ~0 } },
+    { MODKEY|ControlMask,           XK_0,      toggleview,     {.ui = ~0 } },
     */
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
-	{ MODKEY,                       XK_r,      quit,           {.i = 1 } },
+    { MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+    { MODKEY,                       XK_r,      quit,           {.i = 1 } },
 };
 
 /* button definitions */
 /* click can be ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static Button buttons[] = {
-	/* click                event mask      button          function        argument */
-	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
-	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
+    /* click                event mask      button          function        argument */
+    { ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
+    { ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
 };
