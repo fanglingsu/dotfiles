08ed03872857ebdc9a8e3312a89dacb19f6aebdf
Daniel Carl <danielcarl@gmx.de>
2014-10-24 00:47:53 +0200

    Use dmenu with xft support.

--- build/dwm/config.h
+++ build/dwm/config.h
@@ -74,7 +74,7 @@ static const Layout layouts[] = {
 /* commands */
 static const char terminal[]  = "urxvtc";
 static const char scratchpadname[] = "scratchy";
-static const char *dmenucmd[] = { "dmenu-run-recent", "-l 7", "-fn", font, "-nb", colors[0][ColBG], "-nf", colors[0][ColFG],"-sb", colors[1][ColBG], "-sf", colors[1][ColFG], NULL };
+static const char *dmenucmd[] = { "dmenu-run-recent", NULL };
 static const char *termcmd[]  = { terminal, NULL };
 static const char *slockcmd[] = { "slock", NULL };
 static const char *scratchpadcmd[] = { terminal, "-name", scratchpadname, "-geometry", "80x20+350+130", NULL };
