641ba08e48110bbe95c9175f377611057206fca2
Daniel Carl <danielcarl@gmx.de>
2014-10-27 00:09:50 +0100

    Add cmd for password manager to dwm.

--- build/dwm/config.h
+++ build/dwm/config.h
@@ -78,12 +78,14 @@ static const char *dmenucmd[] = { "dmenu-run-recent", NULL };
 static const char *termcmd[]  = { terminal, NULL };
 static const char *slockcmd[] = { "slock", NULL };
 static const char *scratchpadcmd[] = { terminal, "-name", scratchpadname, "-geometry", "80x20+350+130", NULL };
+static const char *dpasscmd[] = { "dpass", NULL };
 
 static Key keys[] = {
     /* modifier                     key        function        argument */
     { MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
     { MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
     { MODKEY|ShiftMask,             XK_z,      spawn,          {.v = slockcmd } },
+    { MODKEY|ShiftMask,             XK_p,      spawn,          {.v = dpasscmd } },
     { MODKEY,                       XK_b,      togglebar,      {0} },
     { MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
     { MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
