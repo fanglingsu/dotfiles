58fc4d9bd0fdb41e2c6e3a4b70378278434960bd
Daniel Carl <danielcarl@gmx.de>
2014-11-07 23:02:08 +0100

    Check if xid of vimb.sh is a tabbed window.

--- bin/vimb.sh
+++ bin/vimb.sh
@@ -4,22 +4,25 @@ xidfile="$HOME/.vimb.xid"
 uri=""
 
 if [ "$#" -gt 0 ]; then
-	uri="$1"
+    uri="$1"
 fi
 
 runtabbed() {
-	tabbed -dn tabbed-vimb -r 2 vimb -e '' "$uri" \
+    tabbed -dn tabbed-vimb -r 2 vimb -e '' "$uri" \
         > "$xidfile" 2>/dev/null &
 }
 
 if [ ! -r "$xidfile" ]; then
-	runtabbed
+    runtabbed
 else
-	xid=$(cat "$xidfile")
-	xprop -id "$xid" >/dev/null 2>&1
-	if [ $? -gt 0 ]; then
-		runtabbed
-	else
-		vimb -e "$xid" "$uri" >/dev/null 2>&1 &
-	fi
+    xid=$(cat "$xidfile")
+    # check if the window with the xid still exists
+    # and is a tabbed window
+    xprop -id "$xid" -notype WM_CLASS | \
+        grep '"tabbed"' >/dev/null 2>&1
+    if [ $? -gt 0 ]; then
+        runtabbed
+    else
+        vimb -e "$xid" "$uri" >/dev/null 2>&1 &
+    fi
 fi
