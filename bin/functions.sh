5c0a5f89001d22a1ec992cf99844654c0a382c11
Daniel Carl <danielcarl@gmx.de>
2016-01-27 12:13:47 +0100

    Added action message functions.

--- bin/functions.sh
+++ bin/functions.sh
@@ -47,3 +47,25 @@ write_debug() {
         echo -e "    [${CC}DEBUG$CDEF] $1" >&2
     fi
 }
+
+action_start() {
+	local msg="$1"
+	local len=${#msg}
+	if [ $len -gt 57 ]; then
+		len=57
+	fi
+	printf "$CEB:: $CEW%.*s... $CDEF" $len "$msg" >&2
+}
+
+action_status() {
+	local msg="[$1]"
+	local maxcol=$(tput cols)
+	if [ $maxcol -gt 70 ]; then
+		maxcol=70
+	fi
+
+	# move cursor to beginning of line
+	echo -ne "\r"
+	tput cuf $(($maxcol-${#msg}))
+    printf "%s\n" "$msg" >&2
+}
