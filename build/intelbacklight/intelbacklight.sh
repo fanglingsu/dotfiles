4c4f4c25ba560e45f621bfd46020316a0382e038
Daniel Carl <danielcarl@gmx.de>
2017-01-12 22:34:02 +0100

    Use percent for screen brightness.

--- build/intelbacklight/intelbacklight.sh
+++ build/intelbacklight/intelbacklight.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-brightness=$1
 maxbrightness=$(cat /sys/class/backlight/intel_backlight/max_brightness)
+brightness=$((($maxbrightness * $1) / 100))
 if [ "$brightness" -le "$maxbrightness" ]; then
     echo "$brightness" > /sys/class/backlight/intel_backlight/brightness
 fi
