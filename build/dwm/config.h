c03734945a2d28cf34483c1ba6c6df7316373117
Daniel Carl <danielcarl@gmx.de>
2017-05-31 01:03:14 +0200

    Use tabs in dwm.

--- build/dwm/config.h
+++ build/dwm/config.h
@@ -22,6 +22,12 @@ static const char *colors[][3]      = {
 	[3]          = { "#06989a", "#111111", "#111111" },
 	[4]          = { "#53a653", "#111111", "#111111" },
 };
+/*   Display modes of the tab bar: never shown, always shown, shown only in */
+/*   monocle mode in presence of several windows.                           */
+/*   Modes after showtab_nmodes are disabled                                */
+enum showtab_modes { showtab_never, showtab_auto, showtab_nmodes, showtab_always};
+static const int showtab = showtab_auto;    /* Default tab bar show mode */
+static const int toptab  = False;           /* False means bottom tab bar */
 
 /* tagging */
 static const char *tags[] = { "term", "dev", "web", "media", "jabber", "mail" };
@@ -84,6 +90,7 @@ static Key keys[] = {
     { MODKEY|ShiftMask,             XK_p,      spawn,          {.v = dpasscmd } },
     { MODKEY|ShiftMask,             XK_z,      spawn,          {.v = slockcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	{ MODKEY,                       XK_w,      tabmode,        {-1} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
@@ -130,5 +137,6 @@ static Button buttons[] = {
 	{ ClkTagBar,            0,                Button3,        toggleview,     {0} },
 	{ ClkTagBar,            MODKEY,           Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,           Button3,        toggletag,      {0} },
+	{ ClkTabBar,            0,              Button1,        focuswin,       {0} },
 };
 
