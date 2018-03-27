be38c887284d26943aa460d41e12e29039371011
Daniel Carl <danielcarl@gmx.de>
2018-03-27 15:26:15 +0200

    Shift+Insert to paste primary clipboard in st.

--- build/st-git/config.h
+++ build/st-git/config.h
@@ -175,7 +175,7 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Home,        zoomreset,      {.f =  0} },
 	{ TERMMOD,              XK_C,           clipcopy,       {.i =  0} },
 	{ TERMMOD,              XK_V,           clippaste,      {.i =  0} },
-	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
+	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
 	{ TERMMOD,              XK_I,           iso14755,       {.i =  0} },
 };
