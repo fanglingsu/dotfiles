efd72beaa14dd6f8b0de77dc58e4266fa6f2a46c
Daniel Carl <danielcarl@gmx.de>
2018-03-28 10:24:11 +0200

    Use default font name also for st.

--- build/st-git/config.h
+++ build/st-git/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "dejavu sans mono:pixelsize=14:antialias=true:autohint=true";
+static char *font = "DejaVuSansMono:pixelsize=14:antialias=true:autohint=true";
 static int borderpx = 1;
 
 /*
