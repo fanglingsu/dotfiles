19a96810ccf92635abafe1054a500ffaad12d2d9
Daniel Carl <danielcarl@gmx.de>
2018-03-28 22:44:13 +0200

    Use my previous terminal colors.

--- build/st-git/config.h
+++ build/st-git/config.h
@@ -85,7 +85,7 @@ unsigned int tabspaces = 8;
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
-	"#111111",
+	"#2e3436",
 	"#cc0000",
 	"#4e9a06",
 	"#c4a000",
@@ -107,8 +107,10 @@ static const char *colorname[] = {
 	[255] = 0,
 
 	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
+	"#b0b0b0",
+	"#eeeeee",
+	"#d3d7cf",
+	"#111111"
 };
 
 
@@ -116,8 +118,8 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 7;
-unsigned int defaultbg = 0;
+unsigned int defaultfg = 258;
+unsigned int defaultbg = 259;
 static unsigned int defaultcs = 256;
 static unsigned int defaultrcs = 257;
 
@@ -462,3 +464,4 @@ static char ascii_printable[] =
 	" !\"#$%&'()*+,-./0123456789:;<=>?"
 	"@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_"
 	"`abcdefghijklmnopqrstuvwxyz{|}~";
+/* vim: set noet: */
